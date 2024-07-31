581f9698f55f14b6c5ba994906f081c398336970
Svetlana Evdokimova <7757331@gmail.com>
2024-07-31 15:11:17 +0300

    feat: solve the task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 
-// You should implement your task here.
-
+// You should implement your task here. 
 module.exports = function towelSort (matrix) {
-  return [];
+  let result = [];
+  if (!matrix) {
+    return [];
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    if (i == 0 || i % 2 == 0) {
+      result.push(...matrix[i]);
+    } else {
+      for (j = matrix[i].length - 1; j >= 0; j--) {
+        result.push(matrix[i][j]);
+      }
+    } 
+  } return result;
 }
